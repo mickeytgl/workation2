f184ae4d45afdb2bca3a72f5fe5397d67a3e6341
Miguel Torres <migueltg93@gmail.com>
2020-05-31 13:05:50 +0200

    Set up apollo client

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +14,44 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+import { useMutation } from "@apollo/react-hooks";
+import React from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { render } from "react-dom";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql"
+});
+
+client
+  .mutate({
+    mutation: gql`
+      mutation SignIn {
+        signInUser(
+          input: {
+            credentials: { email: "mickey@mouse.com", password: "password" }
+          }
+        ) {
+          token
+          user {
+            name
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2> Hello mai frends</h2>
+    </div>
+  </ApolloProvider>
+);
+
+document.addEventListener("turbolinks:load", function() {
+  render(<App />, document.getElementById("root"));
+});
