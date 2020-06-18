60fc212fedab29ffb1f923a28eaf9b0a132a3c96
Miguel Torres <migueltg93@gmail.com>
2020-06-18 13:52:21 +0200

    Added sign in html

--- app/javascript/packs/components/SignUp/SignUp.jsx
+++ app/javascript/packs/components/SignUp/SignUp.jsx
@@ -1,96 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
+import { gql } from "apollo-boost";
+import { useMutation } from "@apollo/react-hooks";
 
-const SignUp = () => (
-  <div class="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-    <div class="max-w-md w-full">
-      <div>
-        <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-          Sign in to your account
-        </h2>
-        <p class="mt-2 text-center text-sm leading-5 text-gray-600">
-          Or
-          <a
-            href="#"
-            class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
-          >
-            start your 14-day free trial
-          </a>
-        </p>
-      </div>
-      <form class="mt-8" action="#" method="POST">
-        <input type="hidden" name="remember" value="true" />
-        <div class="rounded-md shadow-sm">
-          <div>
-            <input
-              aria-label="Email address"
-              name="email"
-              type="email"
-              required
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-              placeholder="Email address"
-            />
-          </div>
-          <div class="-mt-px">
-            <input
-              aria-label="Password"
-              name="password"
-              type="password"
-              required
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-              placeholder="Password"
-            />
-          </div>
-        </div>
+const SIGN_IN = gql`
+  mutation SignIn($email: String!, $password: String!) {
+    signInUser(input: { credentials: { email: $email, password: $password } }) {
+      token
+      user {
+        name
+      }
+    }
+  }
+`;
 
-        <div class="mt-6 flex items-center justify-between">
-          <div class="flex items-center">
-            <input
-              id="remember_me"
-              type="checkbox"
-              class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
-            />
-            <label
-              for="remember_me"
-              class="ml-2 block text-sm leading-5 text-gray-900"
-            >
-              Remember me
-            </label>
-          </div>
-
-          <div class="text-sm leading-5">
+const SignUp = () => {
+  let input;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signInUser, { data }] = useMutation(SIGN_IN);
+  return (
+    <div className="flex items-center justify-center px-4 py-12 bg-gray-50 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md">
+        <div>
+          <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900 leading-9">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-sm text-center text-gray-600 leading-5">
+            Or{" "}
             <a
               href="#"
-              class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+              className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
             >
-              Forgot your password?
+              start your 14-day free trial
             </a>
+          </p>
+        </div>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            signInUser({
+              variables: { email, password }
+            });
+            console.log(data);
+          }}
+          className="mt-8"
+          action="#"
+          method="POST"
+        >
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm">
+            <div>
+              <input
+                aria-label="Email address"
+                name="email"
+                type="email"
+                required
+                className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Email address"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="-mt-px">
+              <input
+                aria-label="Password"
+                name="password"
+                type="password"
+                required
+                className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
           </div>
-        </div>
 
-        <div class="mt-6">
-          <button
-            type="submit"
-            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
-          >
-            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-              <svg
-                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
-                fill="currentColor"
-                viewBox="0 0 20 20"
+          <div className="flex items-center justify-between mt-6">
+            <div className="flex items-center">
+              <input
+                id="remember_me"
+                type="checkbox"
+                className="w-4 h-4 text-indigo-600 form-checkbox transition duration-150 ease-in-out"
+              />
+              <label
+                htmlFor="remember_me"
+                className="block ml-2 text-sm text-gray-900 leading-5"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </span>
-            Sign in
-          </button>
-        </div>
-      </form>
+                Remember me
+              </label>
+            </div>
+
+            <div className="text-sm leading-5">
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+              >
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent group leading-5 rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+            >
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg
+                  className="w-5 h-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" />
+                </svg>
+              </span>
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SignUp;
